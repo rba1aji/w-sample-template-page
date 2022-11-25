d7734129764de884cf0daa8c181b2cebae9b1691
Balaji <balaji@corviduk.com>
2022-11-25 20:50:20 +0530

    page navigation modified

--- src/reducers/routes.js
+++ src/reducers/routes.js
@@ -2,13 +2,14 @@ import Categories from "../pages/Categories";
 import Category from "../pages/Category";
 
 export const routes = [
+    // {
+    //     path: "/",
+    //     element: null,
+    //     title: "Home"
+    // },
     {
-        path: "/",
-        element: null,
-        title: "Home"
-    },
-    {
-        path: '/categories',
+        // path: '/categories',
+        path: '/',
         element: <Categories />,
         title: 'Categories'
     },
